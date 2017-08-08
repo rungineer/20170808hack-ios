cb619d1c787e9557dafe8c995a0aa0b78f9feeda
shido <poporotylm@gmail.com>
2017-08-08 15:12:39 +0900

    first init

---
+++ 20170808hack-ios/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  20170808hack-ios
+//
+//  Created by Daichi Shibata on 2017/08/08.
+//  Copyright © 2017 rungineer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target '20170808hack-ios' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for 20170808hack-ios
+
+  target '20170808hack-iosTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target '20170808hack-iosUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
